28b4902f50e557084b5ed6ce5c81e30203de83aa
MD <88189681+A11yTime@users.noreply.github.com>
2024-04-16 15:39:00 -0700

    Update ModalChild.vue

--- ModalChild.vue
+++ ModalChild.vue
@@ -1,1 +1,43 @@
+<template>
+<div>
 
+ <div
+  >
+  <h2 id="dialog1Title">Sloan Dresser & Topper Set</h2>
+  <p id="dialog1Desc">Description goes here</p>
+  <form @submit.prevent="">
+    <p>
+      <label for="firstName">First Name: </label>
+      <input id="firstName" type="text" />
+    </p>
+    <p>
+      <label for="lastName">Last Name: </label>
+      <input id="lastName" type="text" />
+    </p>
+    <p>
+      <label for="interests">Interests: </label>
+      <textarea id="interests"></textarea>
+    </p>
+    <p>
+      <label for="autoLogin">Auto-login?</label>
+      <input type="checkbox" id="autoLogin" name="autoLogin" />
+    </p>
+    <p>
+      <input type="submit" value="Save Information" />
+    </p>
+  </form>
+</div>
+</div>
+</template>
+<script>
+
+export default {
+  name: 'ModalChild',
+
+  
+
+}
+</script>
+<style scoped>
+
+</style>

c3a06e9e27a35f370a2c9c6a35fea387fb3fb8e7
MD <88189681+A11yTime@users.noreply.github.com>
2024-04-16 15:43:21 -0700

    Update mixins.js

--- mixins.js
+++ mixins.js
@@ -1,1 +1,26 @@
+export default {
+    data() {
+        return {
 
+        }
+    },
+    methods: {
+      
+        trapFocus(event) {
+            const focusableElements = this.$refs.modal.querySelectorAll('button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])');
+            const firstElement = focusableElements[0];
+            const lastElement = focusableElements[focusableElements.length - 1];
+      
+            const isShiftPressed = event.shiftKey;
+            const isTab = (event.key === 'Tab')
+      
+            if (!isShiftPressed && document.activeElement === lastElement && isTab) {
+              event.preventDefault();
+              firstElement.focus();
+            } else if (isShiftPressed && document.activeElement === firstElement && isTab) {
+              event.preventDefault();
+              lastElement.focus();
+            }
+          }
+    }
+}
